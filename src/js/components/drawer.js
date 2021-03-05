b236222110a472812999041276f38f19181b08b4
daiki <daiki.matsuoka.0802@gmail.com>
2021-03-05 19:36:22 +0900

    disabled transition on browser-back in Safari

--- src/js/components/drawer.js
+++ src/js/components/drawer.js
@@ -1,23 +1,34 @@
 import setting from "./setting"
 
-const toggleDrawer = (flg)=>{
-    document.getElementById('header__menu-trigger').classList.toggle('is-active');
-    document.getElementById('page-cover').classList.toggle('is-open');
-    document.getElementById('container').classList.toggle('is-open');
-    document.getElementById('header').classList.toggle('is-open');
-    document.getElementById('header-menu').classList.toggle('is-open');
+let drawerElements = ['header__menu-trigger', 'page-cover', 'container', 'header', 'header-menu'];
+
+const toggleDrawer = flg => {
+    drawerElements.map(id => document.getElementById(id).classList.toggle('is-open'));
     if(flg){
-        document.body.style.overflowX = 'hidden';
         document.body.style.overflowY = 'scroll';
         document.getElementById('header-menu').style.overflowY = 'hidden';
     }else{
-        document.body.style.overflow = 'hidden';
+        let scrollOffsetY = window.pageYOffset;
+        window.scrollTo(0, scrollOffsetY);
+        document.body.style.overflowY = 'hidden';
         document.getElementById('header-menu').style.overflowY = 'scroll';
     }
     return !flg
 }
 
-export default ()=>{    
+export default ()=>{
+    window.addEventListener("pageshow", event => {
+        if(event.persisted){
+            drawerElements.map(id => {
+                let obj = document.getElementById(id);
+                obj.classList.add('transition-disabled');
+            });
+            setTimeout(()=> drawerElements.map(id => {
+                document.getElementById(id).classList.remove('transition-disabled');
+            }),100);
+        }
+    });
+
     let openFlg = false;
     document.getElementById('header__menu-trigger').addEventListener(setting.bindTouchEnd,()=>{
         if(setting.isSmph()){openFlg = toggleDrawer(openFlg);}
